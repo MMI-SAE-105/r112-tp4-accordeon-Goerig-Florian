36f5872e608d44a24153ead369759695faac3949
Goerig-Florian <florian.goerig@edu.univ-fcomte.fr>
2024-11-27 17:00:11 +0100

    referme les détails suite à un clic sur un `summary`

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+accordeon.addEventListener("click", (none) => {
+    accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+})
